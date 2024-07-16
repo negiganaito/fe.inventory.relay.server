bd87415a3cb8e14f240ec9f053b29e42790a8ea1
tienlx97 <tienlx97@gmail.com>
2024-07-17 00:58:01 +0700

    chore: fucking bug

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express, { json } from 'express';
-import queryMap from './s-2-c/queryMap.json' assert { type: 'json' };
+// import queryMap from '@gatsly/s-2-c/queryMap.json' assert { type: 'json' };
 import { GraphQLSchema, graphql } from 'graphql';
 import { QueryType, rootValue } from './graphql/query.js';
 import { Mutation } from './graphql/mutation/index.js';
 import { dataDrivenDependencies } from './graphql/render-on-fetch/js-dependency.js';
+
+import url from 'url';
 
 export const schema = new GraphQLSchema({
   query: QueryType,
@@ -13,6 +15,26 @@ export const schema = new GraphQLSchema({
 const app = express();
 
 app.use(json());
+
+let queryMap;
+
+const fetchQueryMap = async () => {
+  try {
+    const response = await fetch(
+      process.env.NODE_ENV === 'development'
+        ? url.pathToFileURL('../../s-2-c/queryMap.json')
+        : 'https://raw.githubusercontent.com/negiganaito/s-2-c/main/queryMap.json',
+    );
+    if (!response.ok) {
+      throw new Error('Network response was not ok ' + response.statusText);
+    }
+    queryMap = await response.json();
+  } catch (error) {
+    console.error('Failed to fetch query map:', error);
+  }
+};
+
+fetchQueryMap(); // Fetch queryMap initially
 
 const handleCors = (req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
